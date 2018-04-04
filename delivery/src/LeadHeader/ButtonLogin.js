82a72725fc8f69312814c0a25742b107596ed696
AnastasiaMikhailenko <36931986+AnastasiaMikhailenko@users.noreply.github.com>
2018-04-04 17:30:25 +0300

    Update ButtonLogin.js
    Add

--- delivery/src/LeadHeader/ButtonLogin.js
+++ delivery/src/LeadHeader/ButtonLogin.js
@@ -4,10 +4,6 @@ import Button from './Button'
 
 const ButtonLoginStyle = Button.extend`
     margin-top: 63px;
-
-    @media (max-width: 1200px) {
-        margin-top: 16px;
-    }
 `
 const ButtonLogin = () => (
     <ButtonLoginStyle>
